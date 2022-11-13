68b49bd74d60df1f0a99f943123baa4625d0fc56
weiran1999 <907221539@qq.com>
2022-11-13 23:18:20 +0800

    feat: alter RestController

--- src/main/kotlin/com/weiran/lottery/controller/PickController.kt
+++ src/main/kotlin/com/weiran/lottery/controller/PickController.kt
@@ -2,11 +2,11 @@ package com.weiran.lottery.controller
 
 import com.weiran.lottery.service.PickService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class PickController {
 
     @Autowired
